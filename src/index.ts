2ca357b96d1e1015caa3251a4ab801b12c0955ec
Fereshteh <fereshte.namazi.edu@gmail.com>
2024-12-16 16:00:36 +0330

    Update - package content

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ export class AIBrary {
   constructor({ apiKey, baseURL }: AILibraryConfig) {
     this.openai = new OpenAI({
       apiKey,
-      baseURL: baseURL || "https://api.openai.com/v1", // Default OpenAI API URL
+      baseURL: baseURL || "https://api.openai.com/v1",
     });
   }
   async generateImage(
